c7d532b5a02f52a0b90c8967eb7164323d118918
Afeef <afeef9706@gmail.com>
2022-12-27 18:59:19 +0800

    Changed Twitter logo to use icon instead of img

--- scripts/newcolor.js
+++ scripts/newcolor.js
@@ -3,6 +3,7 @@ import colors from "./colors.js";
 function newColor() {
     let item = Math.floor(Math.random()*colors.length);
     $("body, .button").css({'background-color': '#' + colors[item]});
+    $(".twitter-logo").css({'color': '#' + colors[item]});
 }
 
 export default newColor;
